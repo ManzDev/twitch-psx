b0649599d280077882bf52530be5be4de7e4b302
ManzDev <JoseRomanHernandez@gmail.com>
2021-09-26 15:23:55 +0100

    Changes to redeem Twitch

--- src/index.js
+++ src/index.js
@@ -13,7 +13,35 @@ document.body.style.setProperty("--triangle-shape", triangleShape.substring(1));
 
 const audio = new Howl({
   src: ["level1.mp3"],
-  loop: true
+  loop: true,
+  volume: 0.75
 });
 
-audio.play();
+const road = document.querySelector(".road-container");
+const psx = document.querySelector(".play-station-container");
+const [leftController, rightController] = document.querySelectorAll(".play-station-controller-container");
+
+const start = () => {
+  setTimeout(() => {
+    psx.classList.remove("hide");
+  }, 500);
+
+  setTimeout(() => {
+    psx.classList.add("dancing");
+    road.classList.add("animated");
+    audio.play();
+  }, 2000);
+
+  setTimeout(() => {
+    leftController.classList.remove("hide");
+    rightController.classList.remove("hide");
+  }, 11200);
+
+  setTimeout(() => {
+    leftController.classList.add("dancing");
+    rightController.classList.add("dancing");
+  }, 13200);
+};
+
+// document.body.addEventListener("click", () => start(), { once: true });
+setTimeout(() => start(), 3000);
